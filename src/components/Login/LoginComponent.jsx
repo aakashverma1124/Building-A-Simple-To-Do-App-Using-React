cb06493ece2f36e70cc303b9a474ea1f6b4006f1
aakashverma1124 <info.aakash11@gmail.com>
2020-05-17 13:37:34 +0530

    created todoapplication project

---
+++ src/components/Login/LoginComponent.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class LoginComponent extends Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+
+}
+
+export default LoginComponent
